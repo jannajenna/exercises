02cf12a79397833200a7219715d7ad5d7b3d8c80
anna <annabarellaalonso@gmail.com>
2022-09-09 18:20:01 +0200

    files to folders

--- loop.js
+++
@@ -1,9 +0,0 @@
-"user stric";
-
-let counter;
-
-for (let counter = 1; (counter = 10); counter++) {
-  console.log(counter);
-}
-
-console.log(`after the loop, counter is ${counter}`);
---
+++ loops/loop.js
@@ -0,0 +1,51 @@
+"user stric";
+
+let counter;
+
+/* for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+}
+console.log(`after the loop, counter is ${counter}`); */
+
+/* for (let counter = 1; counter < 11; counter++) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
+
+/* for (let counter = 1; counter <= 10; counter++) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
+
+/* for (let counter = 10; counter > 0; counter--) {
+  console.log(counter);
+}
+console.log("liftoff");
+
+for (let counter = 10; counter > 0; counter--) {
+console.log(counter);
+  if( counter ===0){
+  console.log("lift off");
+  }
+} */
+
+/* for (let counter = 1; counter <= 19; counter += 2) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
+
+/* for (let counter = 1; counter = 16777216; counter *= 2) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
+
+/* for (let counter = 111; counter = 138; counter += 3) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
+
+/* for (let counter = 100; counter >= 0; counter -= 10) {
+  console.log(counter);
+  console.log(`after the loop, counter is ${counter}`);
+} */
